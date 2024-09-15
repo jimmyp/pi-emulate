e82ba3341424e9a95b4f119fac4b8a8fd67fb595
Jim Pelletier <mail@thejimmyp.com>
2024-09-15 19:35:04 +1000

    downloads and attempts to start a rbpi image

--- start.sh
+++ start.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# check xz is installed
+if ! [ -x "$(command -v xz)" ]; then
+    echo 'Error: xz is not installed. Install with "sudo apt install xz-utils"' >&2
+    exit 1
+fi
+
+# download a file if it does not exist
+if [ ! -f "boot.iso" ]; then
+    curl -O https://downloads.raspberrypi.com/raspios_arm64/images/raspios_arm64-2024-07-04/2024-07-04-raspios-bookworm-arm64.img.xz
+    xz -d -v 2024-07-04-raspios-bookworm-arm64.img.xz
+    rm -rf raspios_arm64-2024-07-04/2024-07-04-raspios-bookworm-arm64.img.xz
+    mv 2024-07-04-raspios-bookworm-arm64.img boot.iso
+fi
+
+
  #start the docker copmose file in the background but ridirect the output to the terminal
 docker-compose up -d
 
@@ -12,6 +27,7 @@ while true; do
     fi
     sleep 1
     echo -n "."
+done
 
 # open the machine in the browser
 xdg-open http://localhost:8006
